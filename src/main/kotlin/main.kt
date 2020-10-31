a50efff36caa6d74b067f0ca0e37c3f626afbfe9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-31 13:11:42 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,13 @@
 fun main() {
+    var str:String? = "Przemysław"
+
+    println("Długość napisu: ${str?.length}")
+    println("Długość napisu: ${str!!.length}")
+
+    if(true)
+        str = null
+
+    println("Długość napisu: ${str?.length}")
+    println("Długość napisu: ${str!!.length}")
+
 }
