21ec9835510e43d504fe495f24658f3117630d21
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-31 13:18:05 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
+import java.lang.NullPointerException
+import java.lang.NumberFormatException
+
 fun main() {
     var str:String? = "Przemysław"
 
@@ -7,7 +10,27 @@ fun main() {
     if(true)
         str = null
 
-    println("Długość napisu: ${str?.length}")
-    println("Długość napisu: ${str!!.length}")
+    println("Długość napisu: ${str?.length?:-1}")
+
+    try {
+        println("Długość napisu: ${str!!.length}")
+    }catch (e:NullPointerException){
+        println("Przechwycono wyjątek: Null Pointer Exception")
+    }
+
+    var length = 0;
+
+    if(str==null){
+        length = 0
+    }else
+        length = -1
+
+    var length1 = str?.length?:-1
+
+    str?.let{
+        println(it)
+    }
+
+
 
 }
